de731dba8b174fa65795ced20f2c8f7173d1a58d
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-03 11:09:45 +0100

    refactor ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,22 +6,13 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:27:51 by ansebast          #+#    #+#             */
-/*   Updated: 2024/05/29 11:27:54 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/03 11:09:28 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t size)
+void	ft_bzero(void *first, size_t num)
 {
-	size_t			i;
-	unsigned char	*p;
-
-	i = 0;
-	p = (unsigned char *)s;
-	while (i < size)
-	{
-		p[i] = 0;
-		i++;
-	}
+	ft_memset(first, 0, num);
 }
