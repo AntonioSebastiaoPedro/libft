0c5c28fec62f2791f22e778f50a6721f7e4c8bb0
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-03 18:25:17 +0100

    refactor ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,37 +6,26 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:29:56 by ansebast          #+#    #+#             */
-/*   Updated: 2024/05/29 11:29:57 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/03 18:23:43 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t num)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
-
+	unsigned char *d;
+	unsigned char *s;
+	
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
+	if (num == 0 || s == d)
+		return (dest);
 	if (d < s)
-	{
-		i = 0;
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	}
+		while (num--)
+			*d++ = *s++;
 	else
-	{
-		i = n;
-		while (i > 0)
-		{
-			d[i - 1] = s[i - 1];
-			i--;
-		}
-	}
+		while (num--)
+			d[num] + s[num];
 	return (dest);
 }
