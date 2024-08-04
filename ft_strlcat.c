6888d54af335d04432109e36bba060ee76f3d235
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-04 07:17:47 +0100

    refactor ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,36 +6,26 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:31:30 by ansebast          #+#    #+#             */
-/*   Updated: 2024/05/29 11:31:32 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/04 07:17:19 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dest, const char *src, size_t size_dest)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	space_left;
-	size_t	i;
-	char	*s;
+        size_t  dest_len;
+        size_t  remaining;
+        size_t  i;
 
-	s = (char *)src;
-	dst_len = 0;
-	src_len = 0;
-	while (dst_len < size && dst[dst_len] != '\0')
-		dst_len++;
-	while (s[src_len] != '\0')
-		src_len++;
-	if (dst_len == size)
-		return (size + src_len);
-	space_left = size - dst_len;
-	i = 0;
-	while (i < space_left - 1 && src[i] != '\0')
-	{
-		dst[dst_len + i] = src[i];
-		i++;
-	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
+        dest_len = strlen(dest);
+        remaining = size_dest - dest_len - 1;
+        i = 0;
+        while (i < remaining && src[i] != '\0')
+        {
+                dest[dest_len + i] = src[i];
+                i++;
+        }
+        dest[dest_len + i] = '\0';
+        return (dest_len + strlen(src));
 }
