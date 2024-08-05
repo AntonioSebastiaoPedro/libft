ed3c0fc52749843d72415fee8d59a3af28f5f23b
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-05 11:23:45 +0100

    refactor ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,22 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:31:55 by ansebast          #+#    #+#             */
-/*   Updated: 2024/05/29 11:31:57 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/05 11:22:47 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int     ft_strncmp(const char *str1, const char *str2, size_t num)
 {
-	size_t	i;
+        size_t  i;
 
-	if (!n)
-		return (0);
-	i = 0;
-	while (i < (n - 1) && s1[i] == s2[i] && s1[i] && s2[i])
-		i++;
-	return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
+        i = 0;
+        while (i < num)
+        {
+                if (str1[i] != str2[i] || !str1[i] || !str2[i])
+                        return (unsigned char)str1[i] - (unsigned char)str2[i];
+                i++;
+        }
+        return (0);
 }
