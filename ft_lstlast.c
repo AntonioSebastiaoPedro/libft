b125870f7075e3807f3bce34420a7b6f1c4109b7
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-14 08:41:17 +0100

    refactor ft_lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,13 +6,13 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:29:16 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/14 08:21:32 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/14 08:40:58 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *ftlstlast(t_list *lst)
+t_list  *ft_lstlast(t_list *lst)
 {
         while (lst->next)
         {
