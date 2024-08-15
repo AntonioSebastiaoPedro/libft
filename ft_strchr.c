e98970373a08055ce57fc8325d28eedff37ce202
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-15 11:55:26 +0100

    Fix norminette errors

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:30:54 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/15 07:25:38 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/15 11:52:00 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *str, int c)
 {
 	while (*str)
 	{
-                if (*str == (char )c)
+		if (*str == (char)c)
 			return ((char *)str);
-                str++;
-        }
-        if ((char )c == '\0')
-                return ((char *)str);
-        return (NULL);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
 }
