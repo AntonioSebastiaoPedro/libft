83e982cd7f15e40bc30e45d2a4b126c400468403
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-15 08:05:57 +0100

    Fix errors

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:31:30 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/14 19:04:59 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/15 08:05:22 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size_dest)
 {
 	size_t	dest_len;
+	size_t	src_len;
 	size_t	remaining;
 	size_t	i;
 
 	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dest_len >= size_dest)
+		dest_len = size_dest;
+	if (dest_len == size_dest)
+		return (size_dest + src_len);
 	remaining = size_dest - dest_len - 1;
 	i = 0;
 	while (i < remaining && src[i] != '\0')
